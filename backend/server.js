ad85f2833ea82d6c5b6811900eec86a238a087a0
Dilli Basker <dillibasker1@gmail.com>
2026-01-08 16:15:47 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://your-frontend-domain.vercel.app'
+    'https://e-commerce-steel-one.vercel.app/'
   ],
   credentials: true
 }));
